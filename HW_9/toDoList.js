edf73de6253e412fb6bbf5576b4edd1eca8e93eb
Andrey <it.personage@gmail.com>
2023-02-17 17:37:14 -0600

    HW_9_function_addArray

--- HW_9/toDoList.js
+++ HW_9/toDoList.js
@@ -8,84 +8,86 @@
 // Для всего создать функции. Перерисовывать на экране все задачи при любом изменение
 // (например изменился статус или добавили задачу, меняем его в объекте, затем перерисовываем
 // весь список).
+
+
+
 const inputText = document.getElementById("inputText");
 const inputTime = document.getElementById("inputTime");
 const toDoList = document.getElementById("toDoList");
 const btnAdd = document.getElementById("btnAdd");
-
-// const body = document.querySelector("body");
-
-// const form = document.createElement('div');
-// form.classList.add('form');
-// body.append(form);
-
-// const inputText = document.createElement('input');
-// inputText.classList.add('inputText');
-// form.append(inputText)
-
-function getIdToDoListItem(el) {}
+const tasks = [];
 let countId = 0;
 
-function randorElement(inputText, inputTime) {
+function addArray(inputText, inputTime) {
   let inputTextValue = inputText.value;
   let inputTimeValue = inputTime.value;
-
-  if (inputTextValue !== "" && inputTimeValue !== "") {
-    toDoList.classList.add("toDoList");
     countId++;
-
-    const toDoListitem = document.createElement("div");
-    toDoListitem.classList.add("toDo-List-item");
-    toDoListitem.id = countId;
-    toDoList.append(toDoListitem);
-
-    const toDoListBtn = document.createElement("div");
-    toDoListBtn.classList.add("toDo-list-btn");
-    toDoListitem.append(toDoListBtn);
-
-    const inputTextH3 = document.createElement("h3");
-    inputTextH3.classList.add("inputTextH3");
-    inputTextH3.innerHTML = `${inputTextValue}`;
-    toDoListitem.prepend(inputTextH3);
-
-    const inputTimeH3 = document.createElement("h3");
-    inputTimeH3.classList.add("inputTimeH3");
-    inputTimeH3.innerHTML = `Срок ${inputTimeValue} мин`;
-    toDoListBtn.append(inputTimeH3);
-
-    const btnChange = document.createElement("button");
-    btnChange.classList.add("btn-change");
-    btnChange.innerHTML = "Изменить";
-    toDoListBtn.append(btnChange);
-
-    const btnDelete = document.createElement("button");
-    btnDelete.classList.add("btn-delete");
-    btnDelete.innerHTML = "Удалить";
-    toDoListBtn.append(btnDelete);
-
-    btnChange.addEventListener("click", () => {
-      inputText.value = inputTextValue;
-      inputTime.value = inputTimeValue;
-      btnAdd.innerHTML = "Сохранить";
-      btnAdd.addEventListener("click", () => {
-        toDoListitem.remove();
-        btnAdd.innerHTML = "Добавить";
-      });
-    });
-
-    btnDelete.addEventListener("click", () => {
-      toDoListitem.remove();
-      toDoList.classList.remove("toDoList");
-    });
-  }
+    tasks.push({id: countId,
+      name: inputTextValue,
+      time: inputTimeValue,
+    })
+    console.log(tasks);
 }
 
-function changetoDoListitem() {}
+function randerElement(cb, array) {
+
+  if (inputTextValue !== "" && inputTimeValue !== "") {
+    cb()
+
+    // toDoList.classList.add("toDoList");
+    // const toDoListitem = document.createElement("div");
+    // toDoListitem.classList.add("toDo-List-item");
+    // toDoListitem.id = countId;
+    // toDoList.append(toDoListitem);
+
+    // const toDoListBtn = document.createElement("div");
+    // toDoListBtn.classList.add("toDo-list-btn");
+    // toDoListitem.append(toDoListBtn);
+
+    // const inputTextH3 = document.createElement("h3");
+    // inputTextH3.classList.add("inputTextH3");
+    // inputTextH3.innerHTML = `${tasks.inputTextValue}`;
+    // toDoListitem.prepend(inputTextH3);
+
+    // const inputTimeH3 = document.createElement("h3");
+    // inputTimeH3.classList.add("inputTimeH3");
+    // inputTimeH3.innerHTML = `Срок ${tasks.inputTimeValue} мин`;
+    // toDoListBtn.append(inputTimeH3);
+
+    // const btnChange = document.createElement("button");
+    // btnChange.classList.add("btn-change");
+    // btnChange.innerHTML = "Изменить";
+    // toDoListBtn.append(btnChange);
+
+    // const btnDelete = document.createElement("button");
+    // btnDelete.classList.add("btn-delete");
+    // btnDelete.innerHTML = "Удалить";
+    // toDoListBtn.append(btnDelete);
+
+    // btnChange.addEventListener("click", () => {
+    //   inputText.value = tasks.inputTextValue;
+    //   inputTime.value = tasks.inputTimeValue;
+    //   btnAdd.innerHTML = "Сохранить";
+    //   btnAdd.addEventListener("click", () => {
+    //     toDoListitem.remove();
+    //     btnAdd.innerHTML = "Добавить";
+    //   });
+    // });
+
+    // btnDelete.addEventListener("click", () => {
+    //   toDoListitem.remove();
+    //   toDoList.classList.remove("toDoList");
+    // });
+  }
+   
+  }
+
+
 
 btnAdd.addEventListener("click", (event) => {
   event.preventDefault();
 
-  randorElement(inputText, inputTime);
+  randerElement(addArray, tasks);
   inputText.value = "";
   inputTime.value = "";
-});
+})
