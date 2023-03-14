f07866596b75221d13743dbfc01a49c702070c10
Andrei <lebmed9@gmail.com>
2023-03-14 11:39:59 -0600

    lesson_11_50%

--- Lesson_11/lesson_11.js
+++ Lesson_11/lesson_11.js
@@ -11,12 +11,47 @@
 
 //////////////////////////////////////////////
 
-function CreateUser(name, age) {
-    this.name = name;
-    this.age = age;
-}
 
-let firstUser = new CreateUser('Ivan', 21);
-let secondUser = new CreateUser('Alex', 18);
-console.log(firstUser);
-console.log(secondUser);
+// function CreateUser(name, age) {
+//     this.name = name;
+//     this.age = age;
+// }
+
+// CreateUser.prototype.sayHi = function() {
+//     console.log('Меня зовут:' + this.name)
+// }
+
+// let firstUser = new CreateUser('Ivan', 21);
+// let secondUser = new CreateUser('Alex', 18);
+// console.log(firstUser);
+// console.log(secondUser);
+// firstUser.sayHi();
+
+
+//////////////////////////////// Task  start ////////////////////////////////////
+
+
+// function Card(form, to) {
+//     this.form = form;
+//     this.to = to;
+    
+// }
+// Card.prototype.show = function() {
+//     console.log(this.form);
+// }
+
+// Card.prototype.setForm = function(newForm) {
+//     this.form = newForm;
+//     console.log(this);
+// }
+
+// const cardFun = new Card ('1212121212', '2323232323');
+
+// console.log(cardFun);
+
+// cardFun.show();
+// cardFun.setForm('sdsdsd')
+
+
+////////////////////////////////////// Task end /////////////////////////////////////////////////
+
